a0f9a1cd4a42cc96e9c26131d59dd365075b04a4
serdarb <hserdarb@gmail.com>
2019-06-01 15:10:21 +0300

    Add Connection Factory

---
+++ Sources/StandardRepository/Factories/ConnectionFactory.cs
@@ -0,0 +1,41 @@
+﻿using System.Data.Common;
+
+using StandardRepository.Models;
+
+namespace StandardRepository.Factories
+{
+    public class ConnectionFactory<TConnection> : IConnectionFactory<TConnection> where TConnection : DbConnection, new()
+    {
+        private readonly string _connectionString;
+
+        public ConnectionFactory(ConnectionSettings connectionSettings)
+        {
+            _connectionString = GetConnectionString(connectionSettings.DbHost, connectionSettings.DbName, connectionSettings.DbUser, connectionSettings.DbPassword);
+        }
+
+        public ConnectionFactory(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public static string GetConnectionString(string dbHost, string dbName, string dbUser, string dbPassword)
+        {
+            return $"Server={dbHost};Database={dbName};User Id={dbUser};Password={dbPassword};";
+        }
+
+        public static string GetConnectionString(ConnectionSettings connectionSettings)
+        {
+            return GetConnectionString(connectionSettings.DbHost, connectionSettings.DbName, connectionSettings.DbUser, connectionSettings.DbPassword);
+        }
+
+        public TConnection Create()
+        {
+            var connection = new TConnection
+            {
+                ConnectionString = _connectionString
+            };
+            connection.Open();
+            return connection;
+        }
+    }
+}
--- Sources/StandardRepository/Factories/IConnectionFactory.cs
+++ Sources/StandardRepository/Factories/IConnectionFactory.cs
@@ -1,7 +1,9 @@
-﻿namespace StandardRepository.Factories
+﻿using System.Data.Common;
+
+namespace StandardRepository.Factories
 {
-    public interface IConnectionFactory
+    public interface IConnectionFactory<TConnection> where TConnection : DbConnection
     {
-        
+        TConnection Create();
     }
 }
