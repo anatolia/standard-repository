15e5759be02cb8de6a25df7785916b7416879cc7
serdarb <hserdarb@gmail.com>
2019-06-01 15:44:31 +0300

    Create NamingUtilsTests.cs

---
+++ Tests/StandardRepository.Tests/UnitTests/Helpers/NamingUtilsTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using Shouldly;
+
+using StandardRepository.Helpers;
+
+namespace StandardRepository.Tests.UnitTests.Helpers
+{
+    [TestFixture]
+    public class NamingUtilsTests
+    {
+        [TestCase("OrganizationId", "organization_id")]
+        [TestCase("Ip", "ip")]
+        [TestCase("IP", "ip")]
+        [TestCase("Name", "name")]
+        public void GetDelimitedName_Length2_Delimited(string name, string expected)
+        {
+            // Arrange, Act
+            var result = NamingUtils.GetDelimitedName(name);
+
+            // Assert
+            result.ShouldBe(expected);
+        }
+    }
+}
