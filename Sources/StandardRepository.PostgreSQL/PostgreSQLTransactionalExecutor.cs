ad0cb39f70fafd40c7d4ae632675dc721ce24f64
serdarb <hserdarb@gmail.com>
2019-06-01 15:44:23 +0300

    Create PostgreSQLTransactionalExecutor.cs

---
+++ Sources/StandardRepository.PostgreSQL/PostgreSQLTransactionalExecutor.cs
@@ -0,0 +1,13 @@
+﻿using Npgsql;
+
+using StandardRepository.Factories;
+
+namespace StandardRepository.PostgreSQL
+{
+    public class PostgreSQLTransactionalExecutor : TransactionalExecutor<NpgsqlConnection, NpgsqlTransaction>
+    {
+        public PostgreSQLTransactionalExecutor(IConnectionFactory<NpgsqlConnection> connectionFactory) : base(connectionFactory)
+        {
+        }
+    }
+}
