82842f63c8e1c2e8393916c0adbc4bb403b3f8c0
serdarb <hserdarb@gmail.com>
2019-06-01 15:44:14 +0300

    Create PostgreSQLConnectionFactory.cs

---
+++ Sources/StandardRepository.PostgreSQL/Factories/PostgreSQLConnectionFactory.cs
@@ -0,0 +1,18 @@
+﻿using Npgsql;
+
+using StandardRepository.Factories;
+using StandardRepository.Models;
+
+namespace StandardRepository.PostgreSQL.Factories
+{
+    public class PostgreSQLConnectionFactory : ConnectionFactory<NpgsqlConnection>
+    {
+        public PostgreSQLConnectionFactory(ConnectionSettings connectionSettings) : base(connectionSettings)
+        {
+        }
+
+        public PostgreSQLConnectionFactory(string connectionString) : base(connectionString)
+        {
+        }
+    }
+}
