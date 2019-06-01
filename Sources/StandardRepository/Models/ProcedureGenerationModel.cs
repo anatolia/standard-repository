f8e9e11a678b61cf5bc0948133ca055572f3a402
serdarb <hserdarb@gmail.com>
2019-06-01 15:42:59 +0300

    Create ProcedureGenerationModel.cs

---
+++ Sources/StandardRepository/Models/ProcedureGenerationModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace StandardRepository.Models
+{
+    public class ProcedureGenerationModel
+    {
+        public string SchemaName { get; }
+        public string TableName { get; }
+        public string RevisionTableName { get; }
+        public string TableFullName { get; }
+        public string RevisionTableFullName { get; }
+        public string IdFieldName { get; }
+        public string IdParameterName { get; }
+
+        public ProcedureGenerationModel(Type entityType, Func<Type, string> getSchemaName, Func<string, string, bool, string> getNameForDb)
+        {
+            SchemaName = getSchemaName(entityType);
+            TableName = getNameForDb(entityType.Name, null, false);
+            RevisionTableName = $"{TableName}_revision";
+            TableFullName = $"{SchemaName}.{TableName}";
+            RevisionTableFullName = $"{SchemaName}.{RevisionTableName}";
+            IdFieldName = $"{TableName}_id";
+            IdParameterName = $"prm_{IdFieldName}";
+        }
+    }
+}
