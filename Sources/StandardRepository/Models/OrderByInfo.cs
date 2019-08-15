fa451ac75add8dd7b0befa6508272c7d9d550dbf
serdarb <hserdarb@gmail.com>
2019-08-15 19:19:58 +0300

    Create OrderByInfo.cs

---
+++ Sources/StandardRepository/Models/OrderByInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq.Expressions;
+
+using StandardRepository.Models.Entities;
+
+namespace StandardRepository.Models
+{
+    public class OrderByInfo<T> where T : BaseEntity
+    {
+        public Expression<Func<T, object>> OrderByColumn { get; }
+        public bool IsAscending { get; }
+
+        public OrderByInfo(Expression<Func<T, object>> orderByColumn, bool isAscending = true)
+        {
+            OrderByColumn = orderByColumn;
+            IsAscending = isAscending;
+        }
+    }
+}
