e252fc67a04859f0125664668e9c44a9b2ad6813
serdarb <hserdarb@gmail.com>
2019-08-15 19:19:55 +0300

    Create DistinctInfo.cs

---
+++ Sources/StandardRepository/Models/DistinctInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq.Expressions;
+
+using StandardRepository.Models.Entities;
+
+namespace StandardRepository.Models
+{
+    public class DistinctInfo<T> where T : BaseEntity
+    {
+        public Expression<Func<T, object>> DistinctColumn { get; }
+        public bool IsAscending { get; }
+
+        public DistinctInfo(Expression<Func<T, object>> distinctColumn, bool isAscending = true)
+        {
+            DistinctColumn = distinctColumn;
+            IsAscending = isAscending;
+        }
+    }
+}
