28431399c7d61c59818c5baeb310482becf720ce
serdarb <hserdarb@gmail.com>
2019-08-15 19:20:01 +0300

    Create UpdateInfo.cs

---
+++ Sources/StandardRepository/Models/UpdateInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq.Expressions;
+
+using StandardRepository.Models.Entities;
+
+namespace StandardRepository.Models
+{
+    public class UpdateInfo<T> where T : BaseEntity
+    {
+        public Expression<Func<T, object>> UpdateColumn { get; }
+        public object Value { get; }
+
+
+        public UpdateInfo(Expression<Func<T, object>> updateColumn, object value)
+        {
+            UpdateColumn = updateColumn;
+            Value = value;
+        }
+    }
+}
