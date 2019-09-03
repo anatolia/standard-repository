9026d33680b4250b1379fcd4db50fc8b998be42e
Dharmandar Kumar <dipoletechi@gmail.com>
2019-09-03 16:36:44 +0530

    Add new entity files and new unit test cases for mapfields and maprevision methods

---
+++ Tests/StandardRepository.Tests/Base/Entities/Organization.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+using StandardRepository.Models.Entities;
+using StandardRepository.Models.Entities.Schemas;
+
+namespace StandardRepository.Tests.Base.Entities
+{
+    public class Organization : BaseEntity, ISchemaMain
+    {
+        public string Email { get; set; }
+        public string Description { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsSuperOrganization { get; set; }
+        public int ProjectCount { get; set; }
+        public DateTime StartDate { get; set; }
+        public long LongField { get; set; }
+    }
+}
--- Tests/StandardRepository.Tests/UnitTests/Helpers/EntityUtilsTests.cs
+++ Tests/StandardRepository.Tests/UnitTests/Helpers/EntityUtilsTests.cs
@@ -1,76 +1,127 @@
-﻿using NUnit.Framework;
+﻿using System;
+using System.Data;
+using System.Reflection;
+
+using Moq;
+using NUnit.Framework;
+
 using StandardRepository.Helpers;
+using StandardRepository.Models.Entities;
 using StandardRepository.PostgreSQL.Helpers;
 using StandardRepository.Tests.Base.Entities;
 using StandardRepository.Tests.IntegrationTests.Helpers;
-using System.Reflection;
 
 namespace StandardRepository.Tests.UnitTests.Helpers
 {
-
     [TestFixture]
     public class EntityUtilsTests: BaseRepositoryIntegrationTests
     {
-
-        public EntityUtils entity { get; set; }
-        public TestEntity testSchema { get; set; }
+        public EntityUtils SystemUnderTest { get; set; }
+        
         [SetUp]
         public void Run_Before_Every_Test()
         {
-            entity = GetEntityUtils(new PostgreSQLTypeLookup(), Assembly.GetExecutingAssembly());
-            testSchema = new TestEntity();
+            SystemUnderTest = GetEntityUtils(new PostgreSQLTypeLookup(), Assembly.GetExecutingAssembly());          
         }                  
 
         [Test]
         public void Verify_Schema_Name()
-        {            
-          
-            var name=entity.GetSchemaName(testSchema.GetType());
+        {
+            var organization = new Organization();
+            var name=SystemUnderTest.GetSchemaName(organization.GetType());
             Assert.AreEqual(name, "main");
         }
 
         [Test]
         public void Verify_Table_Name()
         {
-            //Inner GetDelimitedName method already has covered all testcases in different
-            //test file, so no need to cover every case here
-            var name = entity.GetTableName(testSchema.GetType());
-            Assert.AreEqual(name, "test_entity");
+            var organization = new Organization();
+            var name = SystemUnderTest.GetTableName(organization.GetType());
+            Assert.AreEqual(name, "organization");
         }
-
 
         [Test]
         public void Verify_Table_Full_Name()
         {
-            var name = entity.GetTableFullName(testSchema.GetType());
-            Assert.AreEqual(name, "main.test_entity");
+            var organization = new Organization();
+            var name = SystemUnderTest.GetTableFullName(organization.GetType());
+            Assert.AreEqual(name, "main.organization");
         }
-
 
         [Test]
         public void Verify_Get_All_Properties()
         {
-            var result = entity.GetAllProperties(testSchema.GetType());
-            //result[0].
-            Assert.AreEqual(result.Length, 14);
+            var organization = new Organization();
+            var result = SystemUnderTest.GetAllProperties(organization.GetType());           
+            Assert.AreEqual(result.Length, 17);
             Assert.AreEqual(result[0].Name, "Email");
-            Assert.AreEqual(result[1].Name, "IsActive");
+            Assert.AreEqual(result[1].Name, "Description");
         }
-
 
         [Test]
         public void Verify_Get_Base_Fields()
         {
-            var result = entity.GetRelatedEntityTypes(testSchema.GetType());
-           
-            Assert.AreEqual(result.Count,1);           
-        }
+            var project = new Project();
+            var result1 = SystemUnderTest.GetRelatedEntityTypes(project.GetType());
 
+            var organization = new Organization();
+            var result2 = SystemUnderTest.GetRelatedEntityTypes(organization.GetType());            
+            Assert.AreEqual(result1.Count,0);
+            Assert.AreEqual(result2.Count, 1);
+        }
 
         [Test]
         public void Verify_MapFields()
         {
-            Assert.IsTrue(true);
+            var organization = new Organization();
+            Mock<IDataReader> dataRecord = new Mock<IDataReader>();
+
+            dataRecord.Setup(reader => reader.FieldCount).Returns(3);
+            dataRecord.Setup(reader => reader[0]).Returns("test@gmail.com");
+            dataRecord.Setup(reader => reader.GetName(0)).Returns("Email");
+            dataRecord.Setup(reader => reader[1]).Returns("this is test case");
+            dataRecord.Setup(reader => reader.GetName(1)).Returns("Description");
+            dataRecord.Setup(reader => reader[2]).Returns(true);
+            dataRecord.Setup(reader => reader.GetName(2)).Returns("IsActive");
+
+            SystemUnderTest.MapFields<Organization>(dataRecord.Object, SystemUnderTest.GetAllProperties(organization.GetType()),SystemUnderTest.GetTableName(organization.GetType()),organization);
+
+            Assert.AreEqual(organization.Email,"test@gmail.com");
+            Assert.AreEqual(organization.Description, "this is test case");
+            Assert.IsTrue(organization.IsActive);
+        }
+
+        [Test]
+        public void Verify_MapFields_Revision()
+        {
+            var organization = new Organization();
+            
+            Mock<IDataReader> dataRecord = new Mock<IDataReader>();
+            
+            dataRecord.Setup(reader => reader.FieldCount).Returns(3);
+            dataRecord.Setup(reader => reader[0]).Returns("test@gmail.com");
+            dataRecord.Setup(reader => reader.GetName(0)).Returns("Email");
+            dataRecord.Setup(reader => reader[1]).Returns("this is test case");
+            dataRecord.Setup(reader => reader.GetName(1)).Returns("Description");
+            dataRecord.Setup(reader => reader[2]).Returns(true);
+            dataRecord.Setup(reader => reader.GetName(2)).Returns("IsActive");
+
+            dataRecord.Setup(reader => reader["organization_id"]).Returns(1);
+            dataRecord.Setup(reader => reader["revision"]).Returns(2);
+            dataRecord.Setup(reader => reader["revisioned_by"]).Returns(10000);
+            var revisionAt = DateTime.UtcNow;
+
+            dataRecord.Setup(reader => reader["revisioned_at"]).Returns(revisionAt);
+
+            var revision = new EntityRevision<Organization>();            
+            SystemUnderTest.MapFieldsRevision<Organization>(dataRecord.Object, SystemUnderTest.GetAllProperties(organization.GetType()), SystemUnderTest.GetTableName(organization.GetType()), revision);
+
+            Assert.AreEqual(revision.Entity.Email, "test@gmail.com");
+            Assert.AreEqual(revision.Entity.Description, "this is test case");
+            Assert.IsTrue(revision.Entity.IsActive);
+            Assert.AreEqual(revision.Id,1);
+            Assert.AreEqual(revision.Revision, 2);
+            Assert.AreEqual(revision.RevisionedAt, revisionAt);
         }
     }
 }
